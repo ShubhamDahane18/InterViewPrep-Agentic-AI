7c1a7c5907af243ede1586ecbdc997e058ec22d3
Pandhari <pandhari2527@gmail.com>
2025-09-17 23:28:00 +0530

    saving and returning qas

--- INTERVIEW/HR/router_node.py
+++ INTERVIEW/HR/router_node.py
@@ -9,33 +9,38 @@ from INTERVIEW.HR.state import HRState
 def router_node(state: HRState) -> Command[Literal["get_user_intent_node", "ask_user_what_next", "hr_round"]]:
     """Route control flow to the right node based on interview state."""
 
+    updated_qa = state.questions_answers.copy()
+
     if state.is_qa:
         qa_pair = {
             "question": state.response,   # system's last asked question
             "answer": state.user_input    # candidate's answer
         }
-        state.questions_answers.setdefault(state.section_name, []).append(qa_pair) 
-    
+        updated_qa.setdefault(state.section_name, []).append(qa_pair)
+
     if state.get_user_intent:
         return Command(
-            goto="get_user_intent_node" , 
+            goto="get_user_intent_node",
             update={
-            "is_qa": False
+                "is_qa": False,
+                "questions_answers": updated_qa
             }
         )
-    
-    elif len(state.questions_answers.get(state.section_name, [])) > 0 and len(state.questions_answers.get(state.section_name, [])) % state.limit == 0:
+
+    elif len(updated_qa.get(state.section_name, [])) > 0 and len(updated_qa.get(state.section_name, [])) % state.limit == 0:
         return Command(
-            goto="ask_user_what_next" , 
+            goto="ask_user_what_next",
             update={
-            "is_qa": False
+                "is_qa": False,
+                "questions_answers": updated_qa
             }
         )
-    
+
     else:
         return Command(
-            goto="hr_round" , 
+            goto="hr_round",
             update={
-                "is_qa": False
+                "is_qa": False,
+                "questions_answers": updated_qa
             }
         )
