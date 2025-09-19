60109379a63011b9186e63876c78467ac2154e85
Pandhari <pandhari2527@gmail.com>
2025-09-19 11:24:50 +0530

    evaluation node upd

--- INTERVIEW/EVALUATION/evaluation_graph.py
+++ INTERVIEW/EVALUATION/evaluation_graph.py
@@ -4,12 +4,12 @@ from INTERVIEW.EVALUATION.nodes import evaluation_node, summary_node, final_repo
 
 def build_evaluation_graph():
     workflow = StateGraph(EvaluationState)
-    workflow.add_node("evaluation", evaluation_node)
+    workflow.add_node("evaluation_node", evaluation_node)
     workflow.add_node("summary", summary_node)
     workflow.add_node("final_report", final_report_node)
 
-    workflow.add_edge(START, "evaluation")
-    workflow.add_edge("evaluation", "summary")
+    workflow.add_edge(START, "evaluation_node")
+    workflow.add_edge("evaluation_node", "summary")
     workflow.add_edge("summary", "final_report")
     workflow.add_edge("final_report", END)
 
